31e6695463b78b7a9444aa570ed369fb63ecd03d
pandasanjay1313-wq <pandasanjay1313@gmail.com>
2026-07-08 19:30:18 +0530

    reactive

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidationErrors, Validators } from '@angular/forms';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { Inputs } from './inputs/inputs';
 import { Lists } from './lists/lists';
@@ -14,16 +14,87 @@ import { CommonModule } from '@angular/common';
 })
 export class App {
 ///Reactive form
-  age = new FormControl(null, [Validators.required, Validators.min(18), Validators.max(45)]);
-  name= new FormControl(null,[Validators.required,Validators.minLength(3),Validators.maxLength(45)]);
-    email= new FormControl(null,[Validators.required,Validators.email]);
+///noAdmin
+ noAdmin(control: AbstractControl): ValidationErrors | null {
+    if (control.value === 'admin' || control.value === 'Admin') {
+      return { noAdmin: true };
+    }
+    return null;
+  }
 
-submit(){
-    this.name.setValue(this.name.value);
-  this.email.setValue(this.email.value);
-  this.age.setValue(this.age.value);
+  //indianPhone number
+indianPhone(control: AbstractControl): ValidationErrors | null{
+  const pattern = /^[6-9]\d{9}$/;
+   if (control.value && !pattern.test(control.value)) {
+      return { indianPhone: true };
+  }
+    return null;
 }
 
+////gmail Only
+ gmailOnly(control: AbstractControl): ValidationErrors | null {
+    if (control.value && !control.value.endsWith('@gmail.com')) {
+      return { gmailOnly: true };
+    }
+    return null;
+  }
+/////password special characters
+specialChar(control: AbstractControl): ValidationErrors | null{
+   const pattern = /[!@#$%^&*(),.?":{}|<>~]/;
+
+    if (control.value && !pattern.test(control.value)) {
+      return { specialChar: true };
+    }
+
+    return null;
+}
+/////confirm password
+ matchPassword(control: AbstractControl): ValidationErrors | null {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('confirmPassword')?.value;
+
+    if (password !== confirmPassword) {
+      return { passwordMismatch: true };
+    }
+
+    return null;
+  }
+
+userForm = new FormGroup({
+  age : new FormControl<number | null>(null, [Validators.required, Validators.min(18), Validators.max(45)]),
+  phone:new FormControl('',[Validators.required, this.indianPhone]),
+  name: new FormControl('',[Validators.required,this.noAdmin]),
+  email: new FormControl('',[Validators.required,Validators.email, this.gmailOnly]),
+  passwordGroup: new FormGroup({
+    password: new FormControl(null,[Validators.required, this.specialChar]),
+    confirmPassword: new FormControl(null,[Validators.required,])
+  },{ validators: this.matchPassword })
+
+})
+  patchData(){
+   this.userForm.patchValue({
+    name:'sanjay',
+    age: 22 ,
+   });
+  }
+
+  setData(){
+    this.userForm.setValue({
+      age: 23,
+      phone: null,
+      name: 'sanjay jaishankar',
+      email: 'sanjay123@gmail.com',
+      passwordGroup: {
+        password: null,
+        confirmPassword: null
+      }
+    })
+  }
+
+
+submit(){
+ if (this.userForm.valid){}
+}
 
 
 //  user = {
